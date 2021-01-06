da7b2b95b47f6d4c339238270fd3a8057f1d5d0a
simtor <simone.ram@ada.ac.uk>
2021-01-06 16:23:28 +0000

    added links

--- linkpusher.js
+++ linkpusher.js
@@ -1,1 +1,5 @@
-//component will be used to selct the link in in the notapad/textpad 
+//component will be used to select the link in in the notapad/textpad 
+
+
+//https://www.geeksforgeeks.org/javascript-program-to-read-text-file/
+//https://stackoverflow.com/questions/14964035/how-to-export-javascript-array-info-to-csv-on-client-side
