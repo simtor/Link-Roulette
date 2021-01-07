e625dc9fda1e3974555b5911d179030c39561002
simtor <simone.ram@ada.ac.uk>
2021-01-07 13:43:50 +0000

    Added a csv file

--- linkpusher.js
+++ linkpusher.js
@@ -1,5 +1,28 @@
 //component will be used to select the link in in the notapad/textpad 
 
 
-//https://www.geeksforgeeks.org/javascript-program-to-read-text-file/
-//https://stackoverflow.com/questions/14964035/how-to-export-javascript-array-info-to-csv-on-client-side
+// const rows = [
+//     ["name1", "city1", "some other info"],
+//     ["name2", "city2", "more info"]
+// ];
+
+// let csvContent = "data:text/csv;charset=utf-8,";
+
+// rows.forEach(function(rowArray) {
+//     let row = rowArray.join(",");
+//     csvContent += row + "\r\n";
+// });
+
+const csv = require('csv-parser');
+const fs = require('fs');
+
+fs.createReadStream('data.csv')
+  .pipe(csv())
+  .on('data', (row) => {
+    console.log(row);
+  })
+  .on('end', () => {
+    console.log('CSV file successfully processed');
+  });
+
+console.log("Simone Ram")
